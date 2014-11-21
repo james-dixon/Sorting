15f34d337efca815daf0a621ca721d981ba92335
James Dixon <james-dixon@users.noreply.github.com>
2014-11-21 00:45:57 +0000

    templated radix sort to work with any unsigned type

--- radix.hpp
+++ radix.hpp
@@ -4,21 +4,23 @@
 #include <vector>
 #include <utility>
 #include <cstdint>
+#include <type_traits>
 
 namespace sort
 {
-
-	void radix(std::vector<uint32_t>& data)
+	template<typename T>
+	typename std::enable_if<std::is_unsigned<T>::value, void>::type
+	radix(std::vector<T>& data)
 	{
-		std::vector<uint32_t> spare(data.size(),0);
-		const uint32_t mask_size = 8;
-		const uint32_t mask = (1<<(mask_size))-1;
-		
-		for(uint32_t depth=0; depth<32; depth += mask_size)
+		std::vector<T> spare(data.size(),0);
+		const uint64_t mask_size = 8;
+		const uint64_t mask = (1<<(mask_size))-1;
+		const uint64_t depth_limit = 8*sizeof(T);
+		for(uint64_t depth=0; depth<depth_limit; depth += mask_size)
 		{
 			const auto start = data.begin();
 			const auto end = data.end();
-			uint32_t totals[mask+1] = {0};
+			uint64_t totals[mask+1] = {0};
 			
 			for(auto itt=start; itt!=end; ++itt)
 			{
@@ -26,13 +28,13 @@ namespace sort
 			}
 
 		
-			for(uint32_t i=mask; i>0; --i)
+			for(uint64_t i=mask; i>0; --i)
 				totals[i] = totals[i-1];
 			totals[0]=0;
-			for(uint32_t i=1; i<=mask; ++i)
+			for(uint64_t i=1; i<=mask; ++i)
 				totals[i] += totals[i-1];
 	
-			uint32_t counts[mask+1] = {0};
+			uint64_t counts[mask+1] = {0};
 			
 			for(auto itt=start; itt!=end; ++itt)
 			{
